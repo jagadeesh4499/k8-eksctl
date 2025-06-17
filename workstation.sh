a491db6882e34872f569e34d16b91d06ac3136fb
jagadeesh <jagadeeshakula123@gmail.com>
2025-06-17 10:56:19 +0530

    k8-eksctl

--- workstation.sh
+++ workstation.sh
@@ -59,10 +59,10 @@ ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 VALIDATE $? "kubens installation"
 
 # Helm
-# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-# chmod 700 get_helm.sh
-# ./get_helm.sh
-# VALIDATE $? "helm installation"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+VALIDATE $? "helm installation"
 
 # k9s
 curl -sS https://webinstall.dev/k9s | bash
